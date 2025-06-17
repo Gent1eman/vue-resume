09adce422dcd8fd27cc97dfe064485927e2b9fdb
hui <191245876@qq.com>
2025-06-17 19:34:44 +0800

    chore: 初始化项目

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+// 对组件进行类型声明;
+declare module "*.vue" {
+    import { DefineComponent } from "vue"
+    const component: DefineComponent
+    export default component
+}
